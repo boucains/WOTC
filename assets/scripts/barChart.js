1faa8947e2e9ffc4645af895edb07b1fa7c7aa18
Jack <jack@jackcain.name>
2016-02-20 23:00:43 -0800

    Updated bar chart from Mike Bostock. Adds a proper y-axis with tick marks that opposite from the bar.

--- assets/scripts/barChart.js
+++ assets/scripts/barChart.js
@@ -3,58 +3,62 @@
  */
 // Bar Chart with Negative Values Code
 
-var margin = {top: 30, right: 10, bottom: 10, left: 10},// jscs:ignore disallowMultipleVarDecl
-  width = 640 - margin.left - margin.right,
-  height = 400 - margin.top - margin.bottom;
+var margin = {top: 20, right: 30, bottom: 40, left: 30},
+  width = 960 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
 
 var x = d3.scale.linear()
   .range([0, width]);
 
 var y = d3.scale.ordinal()
-  .rangeRoundBands([0, height], .2);
+  .rangeRoundBands([0, height], 0.1);
 
 var xAxis = d3.svg.axis()
   .scale(x)
-  .orient('top');
+  .orient("bottom");
 
 var yAxis = d3.svg.axis()
   .scale(y)
-  .orient('right');
+  .orient("left")
+  .tickSize(6, 0);
 
-var svg = d3.select('body').append('svg')
-  .attr('width', width + margin.left + margin.right)
-  .attr('height', height + margin.top + margin.bottom)
-  .append('g')
-  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+var svg = d3.select("body").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv('./assets/data/data.tsv', type, function(error, data) {
+d3.tsv("./assets/data/data.tsv", type, function(error, data) {
   x.domain(d3.extent(data, function(d) { return d.value; })).nice();
   y.domain(data.map(function(d) { return d.name; }));
 
-  svg.selectAll('.bar')
+  svg.selectAll(".bar")
     .data(data)
-    .enter().append('rect')
-    .attr('class', function(d) { return d.value < 0 ? 'bar negative' : 'bar positive'; })
-    .attr('x', function(d) { return x(Math.min(0, d.value)); })
-    .attr('y', function(d) { return y(d.name); })
-    .attr('width', function(d) { return Math.abs(x(d.value) - x(0)); })
-    .attr('height', y.rangeBand());
+    .enter().append("rect")
+    .attr("class", function(d) { return "bar bar--" + (d.value < 0 ? "negative" : "positive"); })
+    .attr("x", function(d) { return x(Math.min(0, d.value)); })
+    .attr("y", function(d) { return y(d.name); })
+    .attr("width", function(d) { return Math.abs(x(d.value) - x(0)); })
+    .attr("height", y.rangeBand());
 
-  svg.append('g')
-    .attr('class', 'x axis')
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
     .call(xAxis);
 
-  svg.append('g')
-    .attr('class', 'y axis')
-    .call(yAxis);
+  var tickNegative = svg.append("g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(" + x(0) + ",0)")
+    .call(yAxis)
+    .selectAll(".tick")
+    .filter(function(d, i) { return data[i].value < 0; });
 
-  svg.append('g')    //puts a vertical line at the zero mark
-    .attr('class', 'y axis')
-    .append('line')
-    .attr('x1', x(0))
-    .attr('x2', x(0))
-    .attr('y2', height);
+  tickNegative.select("line")
+    .attr("x2", 6);
 
+  tickNegative.select("text")
+    .attr("x", 9)
+    .style("text-anchor", "start");
 });
 
 function type(d) {
