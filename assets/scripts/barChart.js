50b01ee6f6738890a35f4c8123a7d080d094b78e
Jack <jack@jackcain.name>
2016-03-02 18:12:01 -0800

    Working Chart in the #barchart div

--- assets/scripts/barChart.js
+++ assets/scripts/barChart.js
@@ -4,8 +4,8 @@
 // Bar Chart with Negative Values Code
 
 var margin = {top: 20, right: 30, bottom: 40, left: 30},
-  width = 960 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
+  width = 720 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
 
 var x = d3.scale.linear()
   .range([0, width]);
@@ -22,7 +22,7 @@ var yAxis = d3.svg.axis()
   .orient("left")
   .tickSize(6, 0);
 
-var svg = d3.select("body").append("svg")
+var chart3 = d3.select("#barChart").append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
@@ -32,7 +32,7 @@ d3.tsv("./assets/data/data.tsv", type, function(error, data) {
   x.domain(d3.extent(data, function(d) { return d.value; })).nice();
   y.domain(data.map(function(d) { return d.name; }));
 
-  svg.selectAll(".bar")
+  chart3.selectAll(".bar")
     .data(data)
     .enter().append("rect")
     .attr("class", function(d) { return "bar bar--" + (d.value < 0 ? "negative" : "positive"); })
@@ -41,12 +41,12 @@ d3.tsv("./assets/data/data.tsv", type, function(error, data) {
     .attr("width", function(d) { return Math.abs(x(d.value) - x(0)); })
     .attr("height", y.rangeBand());
 
-  svg.append("g")
+  chart3.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + height + ")")
     .call(xAxis);
 
-  var tickNegative = svg.append("g")
+  var tickNegative = chart3.append("g")
     .attr("class", "y axis")
     .attr("transform", "translate(" + x(0) + ",0)")
     .call(yAxis)
